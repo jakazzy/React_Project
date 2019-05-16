b43c38a97262acf04bfdf79752302de8439df144
jida <jakazzy@gmail.com>
2019-05-16 04:27:30 +0000

    add  router for pages

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 
 class App extends Component {
   render(){
     return (
-    <div className="App">
-     <Navbar/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Navbar/>
+      </div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+    </BrowserRouter>
   )
   }
   
