d0afc35b8b9fd432ee8720d0c6630608068dd89f
jida <jakazzy@gmail.com>
2019-05-16 04:03:24 +0000

    created About, Contact and Home components

---
+++ poketimes/src/Components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () =>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Quas consectetur voluptatibus repellat facere eum distinctio assumenda quos mollitia! 
+            Numquam ad excepturi accusantium dicta aut officiis ea fuga vel quod! Laboriosam?</p>
+        </div>
+    )
+}
+
+export default About
---
+++ poketimes/src/Components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () =>{
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Quas consectetur voluptatibus repellat facere eum distinctio assumenda quos mollitia! 
+            Numquam ad excepturi accusantium dicta aut officiis ea fuga vel quod! Laboriosam?</p>
+        </div>
+    )
+}
+
+export default Contact
