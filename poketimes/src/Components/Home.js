7a4193577889c092df48c2b3dac71daeef2243e6
jida <jakazzy@gmail.com>
2019-05-16 09:03:23 +0000

    display post from API

--- poketimes/src/Components/Home.js
+++ poketimes/src/Components/Home.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component} from 'react';
+import Axios from 'axios';
 
-const Home = () =>{
-    return(
+
+class Home extends Component {
+
+    state ={
+        posts : []
+    }
+    componentDidMount(){
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((res) =>{
+            this.setState({
+                posts: res.data.slice(0,10)
+            })
+        })
+        }
+
+
+    render(){
+        const { posts } = this.state;
+        const postLists = posts.length ? (
+            posts.map((post)=>{
+                return(
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title"> {post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : 
+        (<div className="center">
+                You have no posts yet
+            </div>
+        )
+
+        return(
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Quas consectetur voluptatibus repellat facere eum distinctio assumenda quos mollitia! 
-            Numquam ad excepturi accusantium dicta aut officiis ea fuga vel quod! Laboriosam?</p>
+            {postLists}
         </div>
     )
+    }
+    
 }
 
 export default Home
